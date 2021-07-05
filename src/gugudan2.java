c851751646b769eee6609053c030bed07ed5c68f
user <user@DESKTOP-7HAK2IN>
2021-07-05 15:55:13 +0900

    이렇게 줄이면??

--- src/gugudan2.java
+++ src/gugudan2.java
@@ -4,19 +4,8 @@ public class gugudan2 {
 		int[] result = new int[9];
 		for(int i = 0; i <result.length ;i++) {
 			result[i] = 2*(i+1);
-		}
-		for(int i=0; i<result.length; i++) {
 			System.out.println(result[i]);
 		}
-		
-		int[] times3 = new int[9]; //times3라는 새로운 배열을 사용
-		for(int i = 0; i <times3.length ;i++) {
-			times3[i] = 3*(i+1);
-		}
-		for(int i=0; i<times3.length; i++) {
-			System.out.println(times3[i]);
-		}
-		
 	}
 
 }
