79ee1494f92217a4c867a889b61595f65aa91dc9
user <user@DESKTOP-7HAK2IN>
2021-07-05 17:02:31 +0900

    예제 1

---
+++ src/practice1.java
@@ -0,0 +1,26 @@
+//	¿¹Á¦ 1
+import java.util.Scanner;
+
+public class practice1 {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int number = scn.nextInt();
+		
+		for(int i = 1; i <= number; i++ ) {
+			System.out.println(i*number);
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
