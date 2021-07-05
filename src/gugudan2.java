dd246ca54140750bd677df67cf63b436ffacb521
user <user@DESKTOP-7HAK2IN>
2021-07-05 16:19:29 +0900

    메소드 활용하여 구구단 다시 구현

--- src/gugudan2.java
+++ src/gugudan2.java
@@ -1,13 +1,22 @@
-
 public class gugudan2 {
+
+	public static int[] calculate(int times) {
+	// 메소드 사용해서 구구단 다시 구현
+		int[] result = new int[9];
+		for(int i = 0; i <result.length ;i++) {
+		result[i] = times*(i+1);
+		}
+		return result;
+	}
+	
 	public static void main(String[] args) {
-		
-		for(int Num = 2; Num <10; Num++ ) {
-			int[] result = new int[9];
-			for(int i = 0; i <result.length ;i++) {
-			result[i] = Num*(i+1);
-			System.out.println(result[i]); //이중 for문 ㅋㅋ 맞는듯?
-			}
+	// 이제 main 메소드에서 calculate 메소드를 사용한다.
+	
+		int[] result = calculate(2);
+				
+		for(int i = 0; i <result.length ;i++) {
+		System.out.println(result[i]); 
 		}
 	}
+	
 }
