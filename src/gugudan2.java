3819e571d294f80e736abd8da2571c89fec46604
user <user@DESKTOP-7HAK2IN>
2021-07-05 16:07:09 +0900

    2중 for 문

--- src/gugudan2.java
+++ src/gugudan2.java
@@ -1,11 +1,13 @@
 
 public class gugudan2 {
 	public static void main(String[] args) {
-		int[] result = new int[9];
-		for(int i = 0; i <result.length ;i++) {
-			result[i] = 2*(i+1);
-			System.out.println(result[i]); //이렇게 줄이고 쓰면 더 간편하지 않나?
+		
+		for(int Num = 2; Num <10; Num++ ) {
+			int[] result = new int[9];
+			for(int i = 0; i <result.length ;i++) {
+			result[i] = Num*(i+1);
+			System.out.println(result[i]); //이중 for문 ㅋㅋ 맞는듯?
+			}
 		}
 	}
-
 }
